1b792aa2cdd1a507746a1a758ed15d24de3d2ac8
VIPer05-png <viperflg05@gmail.com>
2026-03-09 23:40:47 +0530

    daily

--- python/ML/task3.py
+++ python/ML/task3.py
@@ -1,4 +1,16 @@
-import numpy as np
-import pandas as pd
-S= pd.Series(list('abcdef'))
-print(S)
+import cv2, numpy as np, os
+from glob import glob
+
+def load_dataset(data_path, img_size=(100,100)):
+    X, y = [], []
+    persons = sorted(os.listdir(data_path))
+    for label, person in enumerate(persons):
+        person_path = os.path.join(data_path, person)
+        if not os.path.isdir(person_path):
+            continue
+        for img_path in glob(os.path.join(person_path, "*.jpg")):
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+            img = cv2.resize(img, img_size)
+            X.append(img.flatten())
+            y.append(label)
+    return np.array(X), np.array(y), persons
