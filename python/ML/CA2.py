ef466f2e97bea52d6001f78bb416ab1c99bea250
VIPer05-png <viperflg05@gmail.com>
2026-03-11 16:50:14 +0530

    daily

---
+++ python/ML/CA2.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = load_iris()
+
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train,y_train)
+
+y_pred = rf.predict(X_test)
+
+accuracy = accuracy_score(y_test,y_pred)
+print("Random Forest Accuracy:",accuracy)
+
+importance = rf.feature_importances_
+
+plt.bar(X.columns,importance)
+plt.xlabel("Features")
+plt.ylabel("Importance")
+plt.title("Feature Importance - Random Forest")
+plt.xticks(rotation=45)
+plt.show()
